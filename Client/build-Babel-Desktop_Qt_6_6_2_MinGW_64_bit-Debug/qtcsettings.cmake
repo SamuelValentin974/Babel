4342857f8b07ea008c0efadd6eb0393c1fa7eeba
SamuelValentin974 <72002737+SamuelValentin974@users.noreply.github.com>
2024-02-26 11:41:19 +0400

    .

---
+++ Client/build-Babel-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,4 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_CXX_COMPILER" "E:/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "E:/Qt/6.6.2/mingw_64;C:/Qt/6.6.2/mingw_64" CACHE "PATH" "" FORCE)
